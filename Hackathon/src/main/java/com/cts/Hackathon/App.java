0bbe272f31c0150e460ed5b4ae8786e0ae693006
atrima97 <69793856+atrima97@users.noreply.github.com>
2020-08-19 14:36:39 +0530

    Update App.java

--- Hackathon/src/main/java/com/cts/Hackathon/App.java
+++ Hackathon/src/main/java/com/cts/Hackathon/App.java
@@ -1,58 +1,72 @@
-package com.cts.Hackathon;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
+package com.cts.Json;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
+import org.json.simple.*;
+import org.apache.commons.lang3.math.NumberUtils;
+import java.util.*;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+public class Cpu{
+	
 
-public class App 
-{
-    
-	public static void main( String[] args ) throws Exception
-    {
-    	BufferedReader br = new BufferedReader(new FileReader("D:\\CPU.txt"));
-		 String line;
+	public static void main(String args[]) throws Exception
+	{
+		FileReader fr= new FileReader("D:\\CPU.txt");
+		BufferedReader br=new BufferedReader(fr);
+		String f;
 		List<String> list=new ArrayList<String>();
-		 while((line = br.readLine()) != null) {
-		 	String[] columns = line.split(" ");		 	
-		 	list.add(columns[14]);		 	
-		 }
-		 writeJsonSimpleDemo("abcd.json", list);
-		 
-    }
-    
-    
+		while((f=br.readLine())!=null)
+		{
+			
+			String[] c=f.split(" ");
+			//System.out.println(c[14]);
+			list.add(c[14]);
+		}
+		 writeJsonSimpleDemo("Output.json",list);
+	}
+	
+	
+	public static void writeJsonSimpleDemo(String name,List<String> array)throws Exception
+	{
+		JSONArray v=new JSONArray();
+		JSONObject sampletransaction=new JSONObject();
+		JSONObject r=new JSONObject();
+		
+		int a=1;
+		for(String s:array) {
+			if(s!="")
+			{
+		    	 JSONObject so = new JSONObject(); 
 
-	public static void writeJsonSimpleDemo(String filename,List<String> array) throws Exception {
+				so.put(a+"s",s);
+				v.add(so);
+				a=a+1;
+			}
+		}
+		List<Double> n=new ArrayList<Double>();
+		double total=0;
+		for(String str:array)
+		{
+			if(NumberUtils.isNumber(str)) {
+				n.add(Double.parseDouble(str));
+				total=total+Double.parseDouble(str);
+				
+			}
+		}
+		
+		double max=Collections.max(n);
+		double average=total/(n.size());
+		
+		double roundOffMax = Math.round(max * 100.0) / 100.0;
+    	double roundOffAvg = Math.round(average * 100.0) / 100.0;
     	
-    	JSONObject result=new JSONObject();
+    	sampletransaction.put("values", v);
+    	sampletransaction.put("maxcpu", roundOffMax );
+    	sampletransaction.put("avgcpu", roundOffAvg );
+    	r.put("sampletransaction", sampletransaction);
+    	System.out.println(r);
     	
-    	 JSONArray values = new JSONArray();
-    	 int n=1; 
-    	for(String str : array) {
-    		if(str!="") {
-    			JSONObject sampleObject = new JSONObject();    			   		
-    			sampleObject.put( n+"s", str);
-    			values.add(sampleObject);
-    			n=n+1;
-    		}
-    	}    	
-    	
-    	List<Double> num=new ArrayList<Double>();
-    	double sum=0;
-    	
-    	Double max=Collections.max(num);
-    	Double avg=sum/num.size();
-    	
-    	double roundOffMax = Math.round(max * 100.0) / 100.0;
-    	double roundOffAvg = Math.round(avg * 100.0) / 100.0;
-	}	
-    	
+	    Files.write(Paths.get(name),r.toJSONString().getBytes());
+	}
 }
